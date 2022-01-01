5f164123140f2c25b31d1a8503273bf550985644
danil12389 <akverrr@gmail.com>
2022-01-01 14:04:23 +0300

    mvn init and add test-cases

---
+++ apiTesting/src/main/java/com/danila/app/App.java
@@ -0,0 +1,13 @@
+package com.danila.app;
+import java.net.URI;
+import java.net.http.*;
+import java.time.Duration;
+import java.util.Optional;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+          HttpRequest.newBuilder();
+    }
+}
