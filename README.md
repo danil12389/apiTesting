4314bf0b662b1e4c220f596b029aaa4727c29149
danil12389 <akverrr@gmail.com>
2022-01-03 14:30:46 +0300

    finish test-cases, fix readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # apiTesting
 examples of API test-cases
+
+run tests  - mvn clean test -Dtest=com.danila.app.RestTest
+
--- apiTesting/src/test/java/com/danila/app/RestTest.java
+++ apiTesting/src/test/java/com/danila/app/RestTest.java
@@ -3,6 +3,7 @@ package com.danila.app;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import org.junit.jupiter.api.Assertions;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -13,14 +14,22 @@ import java.util.ArrayList;
 
 public class RestTest {
 
-    private final String baseUrl = "https://jsonplaceholder.typicode.com";
+    private final RequestSpecification requestSpec = given().baseUri("https://jsonplaceholder.typicode.com");
 
-    private final RequestSpecification requestSpec = given().baseUri(baseUrl);
+    private final String requestBody =  "{\n" +
+            "  \"title\": \"foo\",\n" +
+            "  \"body\": \"bar\",\n" +
+            "  \"userId\": \"1\" \n}";
+
+    private final String requestBodyBrokenId =  "{\n" +
+            "  \"title\": \"foo\",\n" +
+            "  \"body\": \"bar\",\n" +
+            "  \"userId\": \"1\",\n" +
+            "  \"Id\": \"-1\" \n}";
 
     @Test
     public void getFirstPost() {
         requestSpec.given()
-               .baseUri(baseUrl)
                .when().get("/posts/1")
                .then().statusCode(200)
                .body("userId", equalTo(1))
@@ -31,6 +40,7 @@ public class RestTest {
 
     @Test
     public void getNonExistentPost() {
+        //притворимся, что у нас каждый раз возвращается рандомное количество постов, а не 100 :)
         Response response =
                  requestSpec.
                  given().
@@ -45,5 +55,50 @@ public class RestTest {
         requestSpec.given()
                  .when().get("/posts/"+sizeOfList + 1 +"")
                  .then().statusCode(404);
+    }
+
+    @Test
+    public void getPostBasedOnBrokenUri() {
+        given()
+                .baseUri("https://jsonplaceholder.typicode.com/posts/11%43")
+                .when().get()
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void addPost() {
+        Response response = requestSpec.given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .post("/posts")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(201, response.statusCode());
+        Assertions.assertEquals("foo", response.jsonPath().getString("title"));
+        Assertions.assertEquals("bar", response.jsonPath().getString("body"));
+        Assertions.assertEquals("1", response.jsonPath().getString("userId"));
+        Assertions.assertEquals("101", response.jsonPath().getString("id"));
+    }
+
+    @Test
+    public void addPostWithBrokenId() {
+        Response response = requestSpec.given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBodyBrokenId)
+                .when()
+                .post("/posts")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(201, response.statusCode());
+        Assertions.assertEquals("foo", response.jsonPath().getString("title"));
+        Assertions.assertEquals("bar", response.jsonPath().getString("body"));
+        Assertions.assertEquals("1", response.jsonPath().getString("userId"));
+        Assertions.assertEquals("101", response.jsonPath().getString("id"));
     }
 }
