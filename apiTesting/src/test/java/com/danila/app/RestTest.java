27b75d62f316183a133e6498f9f0c922f465d953
danil12389 <akverrr@gmail.com>
2022-01-03 17:47:52 +0300

    add .gitignore

--- apiTesting/src/test/java/com/danila/app/RestTest.java
+++ apiTesting/src/test/java/com/danila/app/RestTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
